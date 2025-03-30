868bd320670c9835f20c5230758f6d0c582d6a24
Decarp <pilz.simeon@gmail.com>
2025-03-31 00:34:21 +0200

    feat(ceis-backend): first query for /croptop

--- clab_ceis/ceis_backend/main.py
+++ clab_ceis/ceis_backend/main.py
@@ -1,8 +1,45 @@
-
+from utils import get_bindings
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Hello, World!"}
+
+
+@app.get("/croptop")
+def get_info_croptop():
+    try:
+        bindings = get_bindings("getTopData")
+
+        print(bindings)
+        data = [
+            {
+                "recipe": item.get("recipeName", {}).get("value"),
+                "fabricBlockDesign": item.get("fabricBlockDesignName", {}).get("value"),
+                "requiredAmount": int(item.get("requiredAmount", {}).get("value", 0)),
+                "availableAmount": int(item.get("availableAmount", {}).get("value", 0)),
+            }
+            for item in bindings
+        ]
+        print("data", data)
+        return {}
+    except Exception as e:
+        return {"error": str(e)}
+
+    # return {
+    #     "alternatives": [
+    #         {
+    #             "price": 25,
+    #             "co2eq": 33,
+    #             "timestamp": 1707985660,
+    #         },
+    #         {
+    #             "price": 40,
+    #             "co2eq": 20,
+    #             "timestamp": 1708985660,
+    #         },
+    #     ]
+    # }
