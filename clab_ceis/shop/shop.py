fb174cb56bd94cf907ba54d2366cd0ba8175f114
Decarp <pilz.simeon@gmail.com>
2026-02-26 12:33:38 +0100

    chore(ceis-shop): cleanup

--- clab_ceis/shop/shop.py
+++ clab_ceis/shop/shop.py
@@ -2,8 +2,6 @@ from dash import Dash, dcc, html
 from .layouts.home import home_page
 from .layouts.skirt import skirt_page
 from .layouts.top import top_page
-from .layouts.dashboard import dashboard_page
-from .callbacks import skirt_callbacks, top_callbacks, dashboard_callbacks
 from dash.dependencies import Input, Output
 import sys
 from pathlib import Path
@@ -13,7 +11,9 @@ import config
 
 # Initialize the app
 app = Dash(
-    __name__, assets_folder="/app/clab_ceis/assets", suppress_callback_exceptions=True
+    __name__,
+    assets_folder=str(Path(__file__).parent.parent / "assets"),
+    suppress_callback_exceptions=True,
 )
 server = app.server
 
@@ -21,29 +21,6 @@ server = app.server
 app.layout = html.Div(
     children=[
         dcc.Location(id="url", refresh=False),
-        html.Div(
-            className="menu",
-            children=[
-                dcc.Link(
-                    "Home",
-                    href="/",
-                    className="menu-link",
-                    style={"margin-right": "20px"},
-                ),
-                dcc.Link(
-                    "Dashboard",
-                    href="/dashboard",
-                    className="menu-link",
-                    style={"margin-right": "20px"},
-                ),
-            ],
-            style={
-                "background-color": "#f5f5f5",
-                "padding": "10px",
-                "display": "flex",
-                "justify-content": "center",
-            },
-        ),
         html.Div(id="page-content", style={"padding": "20px"}),
     ]
 )
@@ -52,20 +29,12 @@ app.layout = html.Div(
 # Page routing callback
 @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
 def display_page(pathname):
-    if pathname == "/dashboard":
-        return dashboard_page()
-    elif pathname == "/skirt":
+    if pathname == "/skirt":
         return skirt_page()
     elif pathname == "/top":
         return top_page()
     else:
         return home_page()
-
-
-# Import callbacks
-skirt_callbacks.register_callbacks(app)
-top_callbacks.register_callbacks(app)
-dashboard_callbacks.register_callbacks(app)
 
 
 def main():
