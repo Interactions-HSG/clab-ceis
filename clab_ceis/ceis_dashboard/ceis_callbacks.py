d451c6f6f44f4215417af3d2fd4e0e23e410d3de
Decarp <pilz.simeon@gmail.com>
2026-02-26 13:04:23 +0100

    refactor(ceis-shop): moved repair scenairos table to shop

--- clab_ceis/ceis_dashboard/ceis_callbacks.py
+++ clab_ceis/ceis_dashboard/ceis_callbacks.py
@@ -5,7 +5,6 @@ from callbacks import (
     register_co2_callbacks,
     register_dashboard_table_callbacks,
     register_fabric_block_callbacks,
-    register_repair_callbacks,
     register_recipe_type_callbacks,
 )
 
@@ -14,5 +13,4 @@ def get_callbacks(app: Dash, data: ceis_data.CeisData) -> None:
     register_fabric_block_callbacks(app, data)
     register_dashboard_table_callbacks(app, data)
     register_co2_callbacks(app, data)
-    register_repair_callbacks(app)
     register_recipe_type_callbacks(app, data)
